6131c550603c94c9bbb5adee9c55aed723fe0b18
hackinglifeonhtb <132902648+hackinglifeonhtb@users.noreply.github.com>
2024-02-02 01:13:03 +0300

    Update Loading.js

--- tafkeer/src/Loading.js
+++ tafkeer/src/Loading.js
@@ -6,14 +6,14 @@ export default function Loading() {
     const [name, setName] = useState('')
     const [verified, setVerified] = useState(false);
     useEffect(()=>{
-        axios.post(`${process.env.REACT_APP_DB_URI}/api/verify`, {token: localStorage.getItem('token') || ''}, {headers:{'x-access-token':localStorage.getItem('token'), 'email':localStorage.getItem('email')}})
+        axios.post(`https://nodejs3-5e53e7e9c48f.herokuapp.com/api/verify`, {token: localStorage.getItem('token') || ''}, {headers:{'x-access-token':localStorage.getItem('token'), 'email':localStorage.getItem('email')}})
             .then((res)=>{
                 if(res.data.firstName !== undefined && res.data.secondName !== undefined)
                     setVerified(true);
                     setName(res.data.firstName+' '+res.data.secondName)
             }).catch((err)=>{
                 console.log(window.location.href)
-                window.location.replace(`${process.env.REACT_APP_URI}/login?refer_to=${window.location.href}`)
+                window.location.replace(`https://logictubes.org/login?refer_to=${window.location.href}`)
                 console.log(window.location.href)
             })
     }, [])
